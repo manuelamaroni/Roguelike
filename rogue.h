88d497cd0c992ce52c590d5f9f09de992110b6a0
Manuela Maroni <74781629+manuelamaroni@users.noreply.github.com>
2020-12-18 10:31:35 +0100

    Update rogue.h
    Translated in english

--- rogue.h
+++ rogue.h
@@ -3,28 +3,28 @@
 #include<time.h>
 
 typedef struct{
-  int visited; // se la cella è stata visitata, questo valore è 1
-  int value; //valore della cella
-  int xbefore; //x della cella da cui provengo
-  int ybefore; //y della cella da cui provengo
-}cella;
+  int visited; // if the cell has been visited, this value is 1
+  int value; //value of the cell
+  int xbefore; //x of the cell from which I am coming
+  int ybefore; //y of the cell from which I am coming
+}cell;
 
-cella **load_map(int *size, char filename[]); //carica la mappa da file, restituisce la mappa attraverso un doppio puntatore
+cell **load_map(int *size, char filename[]); //loads the map from file, returns the map using a double pointer
 
-void visualize_map(cella **map, int size, int px, int py, int win_size); //stampa a video la mappa
+void visualize_map(cell **map, int size, int px, int py, int win_size); //prints the map
 
-cella **inizializemap(cella **map, int size); //inizializza a 1 la mappa
+cella **initializemap(cell **map, int size); //initializes the map to 1
 
-int generate_random(cella **map, int size, int x, int y); //genera un numero random congruo con la posizione in cui mi trovo
+int generate_random(cell **map, int size, int x, int y); //generates a random number based on the player's position
 
-cella **generate_map(cella **map, int size, int x, int y); // genera casualmente la mappa e la ritorna
+cella **generate_map(cell **map, int size, int x, int y); // generates the map randomly and returns it
 
-void save_map(cella **map, int size, char filename[]); //salva la mappa in un file .txt di nome filename
+void save_map(cell **map, int size, char filename[]); //saves the map on a .txt file named filename
 
-void insert_player_exit_enemies(cella **map, int size); //sceglie casualmente la posizione del giocatore, dell'uscita dal labirinto e dei nemici
+void insert_player_exit_enemies(cell **map, int size); //chooses randomly the player's, the exit's and the enemies' positions
 
-int torno_indietro(cella **map, int size); //restituisce 0 se non ci sono altre strade disponibili
+int go_back(cell **map, int size); //returns 0 if there are not any more paths available
 
-void movimento(cella **map, int size, int xp, int yp, int xe, int ye, int mosse, int mossaprec); //chiede all'utente la mossa e muove il giocatore
+void movement(cell **map, int size, int xp, int yp, int xe, int ye, int moves, int precmove); //asks the user the next move and moves the player
 
-void move_enemies(cella **map, int size); //trova i nemici nella mappa e li muove di una cella
+void move_enemies(cell **map, int size); //finds the enemies and moves them of one cell
